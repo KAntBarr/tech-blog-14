6519578e36723911850cec8beac34f113d1e6510
Koby <kobysac@gmail.com>
2023-12-14 09:15:39 -0800

    refreshed, onto better practices

--- routes_old/apiRoutes/index.js
+++ routes_old/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes.js');
-const postRoutes = require('./postRoutes.js');
-const commentRoutes = require('./commentRoutes.js');
+const userRoutes = require('./userRoutes.js/index.js');
+const postRoutes = require('./postRoutes.js/index.js');
+const commentRoutes = require('./commentRoutes.js/index.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
