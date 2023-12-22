f6a30f9235b3c25f962f13220f67520ea6c2426c
Koby <kobysac@gmail.com>
2023-12-22 00:11:16 -0800

    can show created on date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon');
+
 module.exports = {
     // the helper method 'format_time' will take in a timestamp and return a string with only the time
     format_time: (date) => {
@@ -10,6 +12,23 @@ module.exports = {
         ret += options.fn(i);
       }
       return ret;
+    },
+    returnFormatDate: (timestamp) => {
+      if (timestamp === null) {
+        return 'Sometime';
+      }
+
+      return DateTime.fromMillis(timestamp).toFormat('MM-dd-yyyy hh:mma');
+    
+      // const timestampNumber = typeof timestamp === 'string' ? parseInt(timestamp, 10) : timestamp;
+    
+      // if (typeof timestampNumber === 'number' && !isNaN(timestampNumber)) {
+      //   const date = new Date(timestampNumber);
+      //   return format(date, 'MM-dd-yyyy');
+      // } else {
+      //   // Handle the case where timestamp is not a valid number
+      //   return 'A long time ago in a galaxy far far away....';
+      // }
     }
   };
   
